2c45fa3157fd01e6659f48224ef130d9ebd7f599
Mario Ramos Montesinos <mariodarken@gmail.com>
2019-05-11 17:33:40 +0200

    .

--- src/store/modules/todo/actions.ts
+++ src/store/modules/todo/actions.ts
@@ -1,83 +1,84 @@
-import Vue from 'vue';
-import {ActionContext, ActionTree} from 'vuex';
-import {TodoState, Todo} from '@/store/modules/todo/types';
-import {RootState} from '@/store/types';
-import {AxiosResponse} from 'axios';
+import Vue from "vue";
+import { ActionContext, ActionTree } from "vuex";
+import { TodoState, Todo } from "@/store/modules/todo/types";
+import { RootState } from "@/store/types";
+import { AxiosResponse } from "axios";
 
 type TodoActionContext = ActionContext<TodoState, RootState>;
 type TodoActionTree = ActionTree<TodoState, RootState>;
 
 export const actions: TodoActionTree = {
-    async fetchData(context: TodoActionContext){
-        try {
-            // @ts-ignore
-            const response: AxiosResponse = await Vue.axios({
-                url: '/todos'
-            });
-            const payload: Todo[] = response && response.data;
-            context.commit('todosLoaded', payload);
-        } catch (error) {
-            context.commit('todosError', error.message);
-        } finally {
-            console.log('petición para obtener todos finalizada');
-        }
-    },
-    async addTodo(context: TodoActionContext, todoText: string){
-        try {
-            // @ts-ignore
-            const response: AxiosResponse = await Vue.axios({
-                method: 'POST',
-                url: '/todos',
-                data: {
-                    id: Date.now(),
-                    text: todoText,
-                    done: false
-                }
-            });
-            if(response && response.data){
-                context.dispatch('fetchData');
-            }
-        } catch (error) {
-            context.commit('todosError', error.message);
-        } finally {
-            console.log('petición para insertar todos finalizada');
-        }
-    },
-    async updateTodoStatus(context: TodoActionContext, todo: Todo){
-        try {
-            // @ts-ignore
-            const response: AxiosResponse = await Vue.axios({
-                method: 'PUT',
-                url: '/todos/'+todo.id,
-                data: {
-                    id: todo.id,
-                    text: todo.text,
-                    done: !todo.done
-                }
-            });
-            if(response && response.data){
-                context.dispatch('fetchData');
-            }
-        } catch (error) {
-            context.commit('todosError', error.message);
-        } finally {
-            console.log('petición para updateTodoStatus finalizada');
-        }
-    },
-    async deleteTodoStatus(context: TodoActionContext, todo: Todo){
-        try {
-            // @ts-ignore
-            const {data} = await Vue.axios({ //Es igual que hacer un response=... y luego response.data
-                method: 'DELETE',
-                url: '/todos/'+todo.id
-            });
-            if(data){
-                context.dispatch('fetchData');
-            }
-        } catch (error) {
-            context.commit('todosError', error.message);
-        } finally {
-            console.log('petición para deleteTodoStatus finalizada');
-        }
+  async fetchData(context: TodoActionContext) {
+    try {
+      // @ts-ignore
+      const response: AxiosResponse = await Vue.axios({
+        url: "/todos"
+      });
+      const payload: Todo[] = response && response.data;
+      context.commit("todosLoaded", payload);
+    } catch (error) {
+      context.commit("todosError", error.message);
+    } finally {
+      console.log("petición para obtener todos finalizada");
     }
-}
+  },
+  async addTodo(context: TodoActionContext, todoText: string) {
+    try {
+      // @ts-ignore
+      const response: AxiosResponse = await Vue.axios({
+        method: "POST",
+        url: "/todos",
+        data: {
+          id: Date.now(),
+          text: todoText,
+          done: false
+        }
+      });
+      if (response && response.data) {
+        context.dispatch("fetchData");
+      }
+    } catch (error) {
+      context.commit("todosError", error.message);
+    } finally {
+      console.log("petición para insertar todos finalizada");
+    }
+  },
+  async updateTodoStatus(context: TodoActionContext, todo: Todo) {
+    try {
+      // @ts-ignore
+      const response: AxiosResponse = await Vue.axios({
+        method: "PUT",
+        url: "/todos/" + todo.id,
+        data: {
+          id: todo.id,
+          text: todo.text,
+          done: !todo.done
+        }
+      });
+      if (response && response.data) {
+        context.dispatch("fetchData");
+      }
+    } catch (error) {
+      context.commit("todosError", error.message);
+    } finally {
+      console.log("petición para updateTodoStatus finalizada");
+    }
+  },
+  async deleteTodo(context: TodoActionContext, todo: Todo) {
+    try {
+      // @ts-ignore
+      const { data } = await Vue.axios({
+        //Es igual que hacer un response=... y luego response.data
+        method: "DELETE",
+        url: "/todos/" + todo.id
+      });
+      if (data) {
+        context.dispatch("fetchData");
+      }
+    } catch (error) {
+      context.commit("todosError", error.message);
+    } finally {
+      console.log("petición para deleteTodoStatus finalizada");
+    }
+  }
+};
---
+++ src/views/TodoApp/component.ts
@@ -0,0 +1,26 @@
+import { Component, Vue } from "vue-property-decorator";
+import TodoList from "@/components/TodoList/component";
+import TodoForm from "@/components/TodoForm/component";
+import { Action, Getter, State } from "vuex-class";
+import { Todo } from "@/store/modules/todo/types";
+import Template from "./template.vue";
+const namespace: string = "todoModule";
+
+@Component({
+  components: {
+    TodoList,
+    TodoForm
+  },
+  mixins: [Template]
+})
+export default class TodoApp extends Vue {
+  @Action("fetchData", { namespace }) fetchData!: Function;
+  @Getter("todos", { namespace }) todos!: Todo[];
+  @Getter("done", { namespace }) done!: Todo[];
+  @State("error", { namespace }) errorLoadingTodos!: boolean;
+  @State("errorMessage", { namespace }) errorMessage!: string;
+
+  mounted() {
+    this.fetchData();
+  }
+}
